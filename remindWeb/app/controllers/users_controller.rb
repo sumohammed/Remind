763dc40d6f188d320771f514e86838b303562f18
sumohammed <sumohammed@hotmail.com>
2016-06-14 11:20:46 +0000

    completed signup and login authentication

--- remindWeb/app/controllers/users_controller.rb
+++ remindWeb/app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		log_in @user
   		redirect_to @user
+  		flash[:success] = "Welcome to the Remind"
   	else
   		render 'new'
   	end
--- remindWeb/config/routes.rb
+++ remindWeb/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root            'welcome#index'
-  get 'signup' => 'users#new'
+  get 'sessions/new'
+
+  root                  'welcome#index'
+  get        'signup' => 'users#new'
+  get        'login'  => 'sessions#new'
+  post       'login'  => 'sessions#create'
+  delete     'logout' => 'sessions#destroy'
   resources :users
 end
