0eace06198f6eb88407df521d0dc986ae573a825
sumohammed <sumohammed@hotmail.com>
2016-06-16 16:23:29 +0000

    Finished welcome page

--- remindWeb/config/routes.rb
+++ remindWeb/config/routes.rb
@@ -1,10 +1,10 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																		Rails.application.routes.draw do
   get 'sessions/new'
 
   root                  'welcome#index'
-  get        'signup' => 'users#new'
-  get        'login'  => 'sessions#new'
-  post       'login'  => 'sessions#create'
-  delete     'logout' => 'sessions#destroy'
+  get        :signup => 'users#new'
+  get        :login  => 'sessions#new'
+  post       :login  => 'sessions#create'
+  delete     :logout => 'sessions#destroy'
   resources :users
 end
