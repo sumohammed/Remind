8673ad9b2b45b3ec27174f1d5e227859ebd06fee
sumohammed <sumohammed@hotmail.com>
2016-06-16 21:40:09 +0000

    errors routes

--- remindWeb/config/routes.rb
+++ remindWeb/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																		Rails.application.routes.draw do
   get 'sessions/new'
 
-  root                  'welcome#index'
-  get        :signup => 'users#new'
-  get        :login  => 'sessions#new'
-  post       :login  => 'sessions#create'
-  delete     :logout => 'sessions#destroy'
+  root                   'welcome#index'
+  get        'signup' => 'users#new'
+  get        'login'  => 'sessions#new'
+  post       'login'  => 'sessions#create'
+  delete     'logout' => 'sessions#destroy'
   resources :users
 end
+end
+
