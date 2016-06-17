d5b252366d3a59753d3914651d6fcd14b358d060
sumohammed <sumohammed@hotmail.com>
2016-06-17 10:35:03 +0000

    Rewrote the route.rb file and app now runs

--- remindWeb/config/routes.rb
+++ remindWeb/config/routes.rb
@@ -1,10 +1,8 @@
-Rails.application.routes.draw do																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																		Rails.application.routes.draw do
-  get 'sessions/new'
-
+Rails.application.routes.draw do
   root                  'welcome#index'
-  get        :signup => 'users#new'
-  get        :login  => 'sessions#new'
-  post       :login  => 'sessions#create'
-  delete     :logout => 'sessions#destroy'
+  get        'signup' => 'users#new'
+  get        'login'  => 'sessions#new'
+  post       'login'  => 'sessions#create'
+  delete     'logout' => 'sessions#destroy'
   resources :users
 end
